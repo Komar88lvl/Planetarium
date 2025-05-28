fc7f30437ba02d0fee0ea34dca3e94065c337291
Sviatoslav <djsv91@gmail.com>
2025-05-28 17:12:10 +0300

    Create AstronomyShow model in centauri app
    Model has many-to-many field with ShowTheme

--- centauri/models.py
+++ centauri/models.py
@@ -6,3 +6,15 @@ class ShowTheme(models.Model):
 
     def __str__(self):
         return f"Show theme: {self.name}"
+
+
+class AstronomyShow(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField
+    show_themes = models.ManyToManyField(
+        ShowTheme,
+        related_name="astronomy_shows",
+    )
+
+    def __str__(self):
+        return f"Astronomy show: {self.title}"
