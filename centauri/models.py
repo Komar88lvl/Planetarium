4ae67164c4dcaa748bbfd10866111e171c7ad980
Sviatoslav <djsv91@gmail.com>
2025-05-28 17:45:50 +0300

    Create ShowSession model in centauri app
    models has foreign keys to AstronomyShow & PlanetariumDome models

--- centauri/models.py
+++ centauri/models.py
@@ -30,6 +30,23 @@ class PlanetariumDome(models.Model):
         return f"Hall: {self.name}"
 
 
+class ShowSession(models.Model):
+    astronomy_show = models.ForeignKey(
+        AstronomyShow,
+        on_delete=models.CASCADE,
+        related_name="show_sessions",
+    )
+    planetarium_dome = models.ForeignKey(
+        PlanetariumDome,
+        on_delete=models.CASCADE,
+        related_name="show_sessions",
+    )
+    show_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"show: {self.astronomy_show}, time: {self.show_time}"
+
+
 class Reservation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
@@ -37,3 +54,15 @@ class Reservation(models.Model):
         on_delete=models.CASCADE,
         related_name="reservations",
     )
+
+    def __str__(self):
+        return f"Client name: {self.user}, created: {self.created_at}"
+
+
+# class Ticket(models.Model):
+#     row = models.IntegerField()
+#     seat = models.IntegerField()
+#     show_session = models.ForeignKey(
+#         "ShowSession",
+#
+#     )
