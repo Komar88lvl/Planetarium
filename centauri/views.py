30405db2e26432b48b7a6935855c121ec1d7ec90
Sviatoslav <djsv91@gmail.com>
2025-05-30 17:38:32 +0300

    Create PlanetariumDomeViewSet in centauri.views

--- centauri/views.py
+++ centauri/views.py
@@ -1,9 +1,20 @@
 from rest_framework import viewsets
 
-from centauri.models import ShowTheme
-from centauri.serializers import ShowThemeSerializer
+from centauri.models import (
+    ShowTheme,
+    PlanetariumDome,
+)
+from centauri.serializers import (
+    ShowThemeSerializer,
+    PlanetariumDomeSerializer,
+)
 
 
 class ShowThemeViewSet(viewsets.ModelViewSet):
     queryset = ShowTheme.objects.all()
     serializer_class = ShowThemeSerializer
+
+
+class PlanetariumDomeViewSet(viewsets.ModelViewSet):
+    queryset = PlanetariumDome.objects.all()
+    serializer_class = PlanetariumDomeSerializer
