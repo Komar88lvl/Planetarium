5471fca6dbac255cf10af8eb5ac9e2eed80f816a
Sviatoslav <djsv91@gmail.com>
2025-05-30 17:49:04 +0300

    Create PlanetariumDomeViewSet endpoint in centauri app

--- centauri/urls.py
+++ centauri/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from centauri.views import ShowThemeViewSet
-
+from centauri.views import (
+    ShowThemeViewSet,
+    PlanetariumDomeViewSet,
+)
 
 router = routers.DefaultRouter()
 router.register("show_themes", ShowThemeViewSet)
+router.register("planetarium_domes", PlanetariumDomeViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
