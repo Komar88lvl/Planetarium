35a423476c5d0067eb5871afb5fd79fc54f83661
Sviatoslav <djsv91@gmail.com>
2025-06-02 20:48:17 +0300

    Override gtt_queryset method for ReservationViewSet
    user can see only his reservations

--- centauri/views.py
+++ centauri/views.py
@@ -63,3 +63,6 @@ class ShowSessionViewSet(viewsets.ModelViewSet):
 class ReservationViewSet(viewsets.ModelViewSet):
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
