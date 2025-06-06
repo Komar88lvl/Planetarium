b3e79203712b97c93e12422cc1c1bd6290b18704
Sviatoslav <djsv91@gmail.com>
2025-06-06 15:00:50 +0300

    Add login base auth endpoint to urls.pt in user app

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from rest_framework.authtoken import views
 
 from user.views import CreateUserView
 
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="create")
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", views.obtain_auth_token, name="get_token"),
 ]
 
 app_name = "user"
