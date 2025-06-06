09dc56596e31d0b41d5850c3219e31fd8b4be031
Sviatoslav <djsv91@gmail.com>
2025-06-06 19:11:33 +0300

    Add MEDIA_URL & MEDIA_ROOT to global urls

--- planetarium/urls.py
+++ planetarium/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
 
-urlpatterns = [
+urlpatterns = ([
     path("admin/", admin.site.urls),
     path("api/v1/centauri/", include("centauri.urls", namespace="centauri")),
     path("api/v1/user/", include("user.urls", namespace="user")),
-] + debug_toolbar_urls()
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+               + debug_toolbar_urls())
