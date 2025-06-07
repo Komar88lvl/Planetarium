b12e9a29f23fc71c07b7c6f6f1b439395b95a05b
Sviatoslav <djsv91@gmail.com>
2025-06-07 14:42:10 +0300

    Change User model in user app for use email instead username

--- user/models.py
+++ user/models.py
@@ -1,5 +1,13 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext as _
 
 
 class User(AbstractUser):
-    pass
+    """User model."""
+
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
