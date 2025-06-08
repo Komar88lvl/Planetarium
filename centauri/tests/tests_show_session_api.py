768663e80f74f8b33aab07b8211da816b0301e09
Sviatoslav <djsv91@gmail.com>
2025-06-08 17:18:24 +0300

    Create func sample_astronomy_show for testing in centauri app

--- centauri/tests/tests_show_session_api.py
+++ centauri/tests/tests_show_session_api.py
@@ -1,10 +1,21 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APIClient
 
+from centauri.models import ShowSession, AstronomyShow, ShowTheme
 
 SHOW_SESSION_URL = reverse("centauri:showsession-list")
+
+def sample_astronomy_show(**params) -> AstronomyShow:
+    show_themes = ShowTheme(name="Test show")
+    defaults = {
+        "title": "Sample show",
+        "description": "Sample description",
+        "show_themes": show_themes,
+    }
+    return AstronomyShow.objects.create(**defaults)
 
 
 class UnauthenticatedShowSessionTests(TestCase):
