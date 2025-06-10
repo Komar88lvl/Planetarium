f450873af693b3eb3751588913d2157543fc937b
Sviatoslav <djsv91@gmail.com>
2025-06-10 21:47:13 +0300

    Create UnauthenticatedSAstronomyShowTests in centauri app

--- centauri/tests/tests_astronomy_show_api.py
+++ centauri/tests/tests_astronomy_show_api.py
@@ -6,7 +6,7 @@ from rest_framework.reverse import reverse
 
 from centauri.models import AstronomyShow, ShowTheme
 
-ASTRONOMY_SHOW_URL = reverse("centauri:astronomy_show-list")
+ASTRONOMY_SHOW_URL = reverse("centauri:astronomyshow-list")
 
 
 def sample_astronomy_show() -> AstronomyShow:
@@ -18,3 +18,11 @@ def sample_astronomy_show() -> AstronomyShow:
     )
     astronomy_show.show_themes.add(show_theme_1, show_theme_2)
     return astronomy_show
+
+class UnauthenticatedSAstronomyShowTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        res = self.client.get(ASTRONOMY_SHOW_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
