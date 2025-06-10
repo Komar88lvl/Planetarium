fafc60811a502b9f62b685daaae64cab99c13109
Sviatoslav <djsv91@gmail.com>
2025-06-10 21:44:22 +0300

    Create AstronomyShow tests file & sample_astronomy_show func

---
+++ centauri/tests/tests_astronomy_show_api.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.test import APIClient
+from rest_framework.reverse import reverse
+
+from centauri.models import AstronomyShow, ShowTheme
+
+ASTRONOMY_SHOW_URL = reverse("centauri:astronomy_show-list")
+
+
+def sample_astronomy_show() -> AstronomyShow:
+    show_theme_1 = ShowTheme.objects.create(name="Test show")
+    show_theme_2 = ShowTheme.objects.create(name="Test second show")
+    astronomy_show = AstronomyShow.objects.create(
+        title="Test title",
+        description="Test description",
+    )
+    astronomy_show.show_themes.add(show_theme_1, show_theme_2)
+    return astronomy_show
