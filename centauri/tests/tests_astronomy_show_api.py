299f0149b0e4dd9f4a7a1cbce4c47150745c3ff2
Sviatoslav <djsv91@gmail.com>
2025-06-10 21:58:24 +0300

    Create test_astronomy_show_list for user without admin perm

--- centauri/tests/tests_astronomy_show_api.py
+++ centauri/tests/tests_astronomy_show_api.py
@@ -5,6 +5,7 @@ from rest_framework.test import APIClient
 from rest_framework.reverse import reverse
 
 from centauri.models import AstronomyShow, ShowTheme
+from centauri.serializers import AstronomyShowListSerializer
 
 ASTRONOMY_SHOW_URL = reverse("centauri:astronomyshow-list")
 
@@ -19,6 +20,7 @@ def sample_astronomy_show() -> AstronomyShow:
     astronomy_show.show_themes.add(show_theme_1, show_theme_2)
     return astronomy_show
 
+
 class UnauthenticatedSAstronomyShowTests(TestCase):
     def setUp(self):
         self.client = APIClient()
@@ -26,3 +28,30 @@ class UnauthenticatedSAstronomyShowTests(TestCase):
     def test_auth_required(self):
         res = self.client.get(ASTRONOMY_SHOW_URL)
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class AuthenticatedAstronomyShowTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            email="test@test.test",
+            password="testpassword",
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_astronomy_show_list(self):
+        show_theme_1 = ShowTheme.objects.create(name="Test second")
+        show_theme_2 = ShowTheme.objects.create(name="Test third show")
+        astronomy_show = sample_astronomy_show()
+        astronomy_show_2 = AstronomyShow.objects.create(
+        title="second title",
+        description="second description",
+        )
+        astronomy_show.show_themes.add(show_theme_1, show_theme_2)
+
+        res = self.client.get(ASTRONOMY_SHOW_URL)
+
+        serializer = AstronomyShowListSerializer([astronomy_show, astronomy_show_2], many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data["results"], serializer.data)
