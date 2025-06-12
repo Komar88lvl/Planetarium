ef257c380084a99eaf7f7ae06bfc31d6c42bc35d
Sviatoslav <djsv91@gmail.com>
2025-06-12 16:07:31 +0300

    Change JWT access token lifetime to 1 hour

--- planetarium/settings.py
+++ planetarium/settings.py
@@ -179,7 +179,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
 }
